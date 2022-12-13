b8d69e1759d56398121b5ee8e7c4e186ed71f56b
jvam90 <jvam90@gmail.com>
2022-12-13 17:24:49 -0300

    Add DTO

--- src/anime/anime.controller.ts
+++ src/anime/anime.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { AnimeService } from './anime.service';
+import { AnimeDto } from './dto';
 
 @Controller('anime')
 export class AnimeController {
@@ -9,7 +10,7 @@ export class AnimeController {
   constructor(private animeService: AnimeService) {}
 
   @Get('/')
-  getAnimes(@Body() dto: any) {
+  getAnimes(@Body() dto: AnimeDto) {
     console.log(dto);
     return this.animeService.getAnimes();
   }
---
+++ src/anime/dto/anime.dto.ts
@@ -0,0 +1,5 @@
+export interface AnimeDto {
+  name: string;
+  progress: string;
+  type: string;
+}
